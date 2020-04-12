7302d55d46ccb37ef2c682f4e43f1eab5bed1b27
masaibar <masaibar.dev@gmail.com>
2020-04-12 21:38:03 +0900

    getContentのLauncherを試す

--- app/src/main/java/com/masaibar/activityresultcontractsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/activityresultcontractsample/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.masaibar.activityresultcontractsample
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +15,20 @@ class MainActivity : AppCompatActivity() {
                 Log.d("MainActivity", activityResult.toString())
             }
 
+    private val getContentLauncher = prepareCall(ActivityResultContracts.GetContent()) { uri ->
+        Log.d("MainActivity", "uri: $uri")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button_open.setOnClickListener {
             launcher.launch(SecondActivity.createIntent(this))
+        }
+
+        button_get_content.setOnClickListener {
+            getContentLauncher.launch("image/*")
         }
     }
 }
